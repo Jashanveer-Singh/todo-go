65d4a0b52505f9884c6d8541574ee0ba56e65095
Jashanveer-Singh <puniajashan85@gmail.com>
2025-09-06 06:06:21 +0000

    corrected name from respository to repository and other bug fixes

---
+++ internal/adpaters/repository/file/taskRepo.go
@@ -0,0 +1,181 @@
+package file
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/Jashanveer-Singh/todo-go/internal/errr"
+	"github.com/Jashanveer-Singh/todo-go/internal/models"
+)
+
+func NewTaskRepo(fp string) *taskRepo {
+	_, err := os.Stat(fp)
+	if os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "file: %s does not exist", fp)
+	}
+	if os.IsPermission(err) {
+		fmt.Fprintf(os.Stderr, "not enough permissions for file: %s", fp)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can't use the file: %s\n%s", fp, err.Error())
+		os.Exit(1)
+	}
+
+	return &taskRepo{
+		mu: sync.RWMutex{},
+		fp: fp,
+	}
+}
+
+func newRepoErr(err error) error {
+	return errors.New(fmt.Sprint("Repo Error: ", err.Error()))
+}
+
+type taskRepo struct {
+	mu sync.RWMutex
+	fp string
+}
+
+func (tr *taskRepo) getTasks() ([]models.Task, error) {
+	// tr.mu.RLock()
+	// defer tr.mu.RUnlock()
+
+	tasks := make([]models.Task, 0)
+
+	taskjson, err := os.ReadFile(tr.fp)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read Tasks from file.\n%s", err.Error())
+	}
+	if len(taskjson) != 0 {
+
+		err = json.Unmarshal(taskjson, &tasks)
+		if err != nil {
+			return nil, fmt.Errorf("unable to unmarshal/decode json.\n%s", err.Error())
+		}
+	}
+
+	return tasks, nil
+}
+
+func (tr *taskRepo) write(tasks []models.Task) error {
+	taskjson, err := json.Marshal(tasks)
+	if err != nil {
+		return fmt.Errorf("unable to marshal tasks to json.\n%s", err.Error())
+	}
+
+	// tr.mu.Lock()
+	// defer tr.mu.Unlock()
+
+	err = os.WriteFile(tr.fp, taskjson, 0644)
+	if err != nil {
+		return fmt.Errorf("unable to write tasks to file.\n%s", err.Error())
+	}
+
+	return nil
+}
+
+func (tr *taskRepo) CreateTask(task models.Task) *errr.AppError {
+	task.Status = 0
+	task.ID = time.Now().Unix()
+	tr.mu.Lock()
+	defer tr.mu.Unlock()
+	tasks, err := tr.getTasks()
+	if err != nil {
+		return errr.NewUnexpectedError("Unable to create task due to internal server error")
+	}
+
+	tasks = append(tasks, task)
+
+	err = tr.write(tasks)
+	if err != nil {
+		return errr.NewUnexpectedError("Unable to create task due to internal server error")
+	}
+
+	return nil
+}
+
+func (tr *taskRepo) UpdateTask(id int64, task models.Task) *errr.AppError {
+	tr.mu.Lock()
+	defer tr.mu.Unlock()
+	tasks, err := tr.getTasks()
+	if err != nil {
+		return errr.NewUnexpectedError("Unable to create task due to internal server error")
+	}
+
+	notFound := true
+
+	for i := range tasks {
+		if tasks[i].ID == id {
+			notFound = false
+			// tasks[i] = task
+			if len(task.Title) != 0 {
+				tasks[i].Title = task.Title
+			}
+			if len(task.Desc) != 0 {
+				tasks[i].Desc = task.Desc
+			}
+			if task.Status == 0 || task.Status == 1 {
+				tasks[i].Status = task.Status
+			}
+			break
+		}
+	}
+
+	if notFound {
+		return errr.NewNotFoundError("no task found with id")
+	}
+
+	err = tr.write(tasks)
+	if err != nil {
+		return errr.NewUnexpectedError("Unable to create task due to internal server error")
+	}
+
+	return nil
+}
+
+func (tr *taskRepo) DeleteTask(id int64) *errr.AppError {
+	tr.mu.Lock()
+	defer tr.mu.Unlock()
+	tasks, err := tr.getTasks()
+	if err != nil {
+		return errr.NewUnexpectedError("Unable to create task due to internal server error")
+	}
+
+	notFound := true
+
+	for i := range tasks {
+		if tasks[i].ID == id {
+			notFound = false
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			break
+		}
+	}
+
+	if notFound {
+		return errr.NewUnexpectedError("No task found with id")
+	}
+
+	err = tr.write(tasks)
+	if err != nil {
+		return errr.NewUnexpectedError("Unable to create task due to internal server error")
+	}
+
+	return nil
+}
+
+func (tr *taskRepo) GetTasks() ([]models.Task, *errr.AppError) {
+	tr.mu.RLock()
+	tr.mu.RUnlock()
+
+	tasks, err := tr.getTasks()
+	if err != nil {
+		return nil, errr.NewUnexpectedError("Unable to create task due to internal server error")
+	}
+
+	return tasks, nil
+}
