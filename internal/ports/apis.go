271e538240cad059f77f72572891f30c85672a5e
jass <puniajashan85@gmail.com>
2025-09-08 00:37:51 +0530

    simplified apiserver interface

--- internal/ports/apis.go
+++ internal/ports/apis.go
@@ -1,17 +1,5 @@
 package ports
 
-import "github.com/Jashanveer-Singh/todo-go/internal/models"
-
-type SendResponse = func(Response)
-
-type apiServer interface {
-	ListenCreateTaskRequest() (models.TaskRequestDto, SendResponse)
-	ListenUpdatetaskRequest() (models.TaskRequestDto, SendResponse)
-	ListenDeleteTaskRequest() (models.TaskRequestDto, SendResponse)
-	ListenGettaskRequest() SendResponse
-}
-
-type Response struct {
-	StatusCode int
-	Body       string
+type APIServer interface {
+	ListenAndServe(addr string)
 }
