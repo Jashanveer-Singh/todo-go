1e25b8f1beffeb05be8201906e626038388bd38f
jass <puniajashan85@gmail.com>
2025-09-08 00:50:50 +0530

    added tests for models

---
+++ internal/models/taskDto_test.go
@@ -0,0 +1,99 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestTaskRequestDto_IsValidStatus(t *testing.T) {
+	tests := []struct {
+		name    string // description of this test case
+		taskreq TaskRequestDto
+		want    bool
+	}{
+		{
+			name: "status is pending",
+			taskreq: TaskRequestDto{
+				Status: "Pending",
+			},
+			want: true,
+		},
+		{
+			name: "status is done",
+			taskreq: TaskRequestDto{
+				Status: "Done",
+			},
+			want: true,
+		},
+		{
+			name: "status is invalid",
+			taskreq: TaskRequestDto{
+				Status: "Pend",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.taskreq.IsValidStatus()
+			if got != tt.want {
+				t.Errorf("IsValidStatus() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTaskRequestDto_ToTask(t *testing.T) {
+	tests := []struct {
+		name    string // description of this test case
+		taskreq TaskRequestDto
+		want    Task
+	}{
+		{
+			name: "task in pending",
+			taskreq: TaskRequestDto{
+				Title:  "title",
+				Desc:   "desc",
+				Status: "Pending",
+			},
+			want: Task{
+				Title:  "title",
+				Desc:   "desc",
+				Status: 0,
+			},
+		},
+		{
+			name: "task in done",
+			taskreq: TaskRequestDto{
+				Title:  "title",
+				Desc:   "desc",
+				Status: "Done",
+			},
+			want: Task{
+				Title:  "title",
+				Desc:   "desc",
+				Status: 1,
+			},
+		},
+		{
+			name: "task in invalid status",
+			taskreq: TaskRequestDto{
+				Title:  "title",
+				Desc:   "desc",
+				Status: "asdfj",
+			},
+			want: Task{
+				Title:  "title",
+				Desc:   "desc",
+				Status: -1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.taskreq.ToTask()
+			if got != tt.want {
+				t.Errorf("ToTask() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ internal/models/task_test.go
@@ -0,0 +1,155 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestTask_IsValidTask(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		want bool
+		task Task
+	}{
+		{
+			name: "task is valid",
+			want: true,
+			task: Task{
+				ID:     123,
+				Title:  "title",
+				Desc:   "desc",
+				Status: 1,
+			},
+		},
+		{
+			name: "invalid title",
+			want: false,
+			task: Task{
+				ID:     123,
+				Title:  "",
+				Desc:   "desc",
+				Status: 1,
+			},
+		},
+		{
+			name: "invalid desc",
+			want: false,
+			task: Task{
+				ID:     123,
+				Title:  "title",
+				Desc:   "",
+				Status: 1,
+			},
+		},
+		{
+			name: "invalid status`",
+			want: false,
+			task: Task{
+				ID:     123,
+				Title:  "title",
+				Desc:   "desc",
+				Status: -91,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.task.IsValidTask()
+			if got != tt.want {
+				t.Errorf("IsValidTask() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTask_StatusAsText(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		want string
+		task Task
+	}{
+		{
+			name: "status Pending",
+			want: "Pending",
+			task: Task{
+				Status: 0,
+			},
+		},
+		{
+			name: "status Done",
+			want: "Done",
+			task: Task{
+				Status: 1,
+			},
+		},
+		{
+			name: "status invalid",
+			want: "Invalid Status",
+			task: Task{
+				Status: -1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.task.StatusAsText()
+			if tt.want != got {
+				t.Errorf("StatusAsText() = %v, want %v.", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTask_IsValidStatus(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		task Task
+		want bool
+	}{
+		{
+			name: "status pending",
+			task: Task{},
+			want: true,
+		},
+		{
+			name: "status is done",
+			task: Task{
+				Status: 1,
+			},
+			want: true,
+		},
+		{
+			name: "status is invalid",
+			task: Task{
+				Status: -1,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.task.IsValidStatus()
+			if tt.want != got {
+				t.Errorf("IsValidStatus() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTask_ToDto(t *testing.T) {
+	ta := Task{
+		Title:  "task title",
+		Desc:   "task desc",
+		ID:     12345,
+		Status: 1,
+	}
+	want := TaskResponseDto{
+		Title:  "task title",
+		Desc:   "task desc",
+		ID:     "12345",
+		Status: "Done",
+	}
+	got := ta.ToDto()
+	if got != want {
+		t.Errorf("ToDto() = %v, want %v", got, want)
+	}
+}
