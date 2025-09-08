b6d6cf2b6e8fb9ff558dbf75aa54125401513261
Jashanveer-Singh <puniajashan85@gmail.com>
2025-09-08 11:45:47 +0000

    updated task models to use user

--- internal/models/task.go
+++ internal/models/task.go
@@ -7,6 +7,7 @@ type Task struct {
 	Title  string `json:"title"`
 	Desc   string `json:"desc"`
 	Status int    `json:"status"`
+	UserID int64  `json:"user_id"`
 }
 
 func (t Task) IsValidTask() bool {
