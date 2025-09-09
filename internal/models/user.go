43f46b9644e25ecd3e1d87e34b6dba4343d7de47
Jashanveer-Singh <puniajashan85@gmail.com>
2025-09-09 14:30:03 +0000

    fix: added tags to user struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	ID       int64
-	Username string
-	Password string
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func (u User) IsValidUser() bool {
