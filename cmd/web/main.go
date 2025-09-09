74a3b3fb3c1cad8781e18e1305a128dbf429d24e
Jashanveer-Singh <puniajashan85@gmail.com>
2025-09-09 15:33:10 +0000

    updated main to support changes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,30 +5,40 @@ import (
 	"log"
 	"os"
 	"path"
+	"time"
 
 	"github.com/Jashanveer-Singh/todo-go/internal/adpaters/apis/http"
+	"github.com/Jashanveer-Singh/todo-go/internal/adpaters/bcrypt"
+	"github.com/Jashanveer-Singh/todo-go/internal/adpaters/jwttoken"
 	"github.com/Jashanveer-Singh/todo-go/internal/adpaters/repository/file"
 	"github.com/Jashanveer-Singh/todo-go/internal/services"
 )
 
 func main() {
-	homeDir, err := os.UserHomeDir()
+	cwd, err := os.Getwd()
 	if err != nil {
-		fmt.Fprintf(
-			os.Stderr,
-			"Can't get the home directory, the env variable for home directory may not be set\n",
-		)
+		fmt.Fprintf(os.Stderr, "Can't get the current working directory\n")
 		os.Exit(1)
 	}
+	dirPath := path.Join(cwd, "data")
 
-	tasksFile := path.Join(homeDir, ".local", "todo", "tasks.json")
-	usersFile := path.Join(homeDir, ".local", "todo", "users.json")
+	tasksFile := path.Join(dirPath, "tasks.json")
+	usersFile := path.Join(dirPath, "users.json")
 
 	taskRepo := file.NewTaskRepo(tasksFile)
 	userRepo := file.NewUserRepo(usersFile)
+	jwtTokenProvider := jwttoken.NewJWTTokenProvider(
+		"my secret key",
+		"issuer",
+		"audience",
+		time.Hour*24,
+	)
+	bcryptPasswordHasher := bcrypt.NewBcryptPasswordHasher(10)
+
+	authService := services.NewAuthService(userRepo, jwtTokenProvider, bcryptPasswordHasher)
 	taskService := services.NewTaskService(taskRepo)
-	userService := services.NewUserService(userRepo)
-	apiServer := http.NewHttpServer(taskService, userService)
+	userService := services.NewUserService(userRepo, bcryptPasswordHasher)
+	apiServer := http.NewHttpServer(taskService, userService, authService, jwtTokenProvider)
 
 	log.Println("Starting Server at port:8080")
 	apiServer.ListenAndServe(":8080")
